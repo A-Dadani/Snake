2928641dee6fe196e668bb05a0de0f047316f6f6
UBoiii <der_uboiii@outlook.com>
2022-06-23 10:16:21 +0100

    Add project files.

---
+++ Snake/Snake.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Snake.ino
+ Created:	6/23/2022 10:15:21 AM
+ Author:	der_u
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
